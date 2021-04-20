e60eac36861c28a06edc78c42642a6942143039b
leejihui <dlwlgml20@yanadoo.co.kr>
2021-04-20 22:50:16 +0900

    210420 - 이중 map 수정 mainNav / subNav 분리 작업

--- yanadoo/src/components/common/nav.jsx
+++ yanadoo/src/components/common/nav.jsx
@@ -24,13 +24,42 @@ let navList = [
     }
 ]
 
+let subList = [
+    {
+        sub1: [
+            {id: 1, desc: 'All jobs 서브1'},
+            {id: 2, desc: 'All jobs 서브2'},
+            {id: 3, desc: 'All jobs 서브3'},
+            {id: 4, desc: 'All jobs 서브4'},
+        ],
+        sub2: [
+            {id: 1, desc: 'Events 서브1'},
+            {id: 2, desc: 'Events 서브2'},
+            {id: 3, desc: 'Events 서브3'},
+            {id: 4, desc: 'Events 서브4'},
+        ],
+        sub3: [
+            {id: 1, desc: 'Resume 서브1'},
+            {id: 2, desc: 'Resume 서브2'},
+            {id: 3, desc: 'Resume 서브3'},
+            {id: 4, desc: 'Resume 서브4'},
+        ],
+        sub4: [
+            {id: 1, desc: 'MathUp 서브1'},
+            {id: 2, desc: 'MathUp 서브2'},
+            {id: 3, desc: 'MathUp 서브3'},
+            {id: 4, desc: 'MathUp 서브4'},
+        ]
+    }
+]
+
 const Nav = () => {
     const [navHover, setNavHover] = useState(false);
     
     return (
         <nav className="nav-box">
             <ul className="main-nav-box">
-            {
+            {/* {
                 navList.map((mainNav, idx) => (
                     <li key={mainNav.id}>
                         <a href="#" onMouseOver={() => {setNavHover(true)}} onMouseLeave={() => {setNavHover(false)}}>{mainNav.mainNav}</a>
@@ -40,6 +69,28 @@ const Nav = () => {
                                    <li key={idx}>
                                        <a href="#">{subNav}</a>
                                    </li>
+                                  
+                               ))
+                           }
+                        </ul>
+                    </li>
+                ))
+            } */}
+
+            {
+                navList.map(mainNav => (
+                    <li key={mainNav.id}>
+                        <a href="#" onMouseEnter={() => {setNavHover(true)}} onMouseLeave={() => {setNavHover(false)}}>{mainNav.mainNav}</a>
+                        <ul  className={navHover === true  ? 'sub-nav-box active' : 'sub-nav-box'}>
+                           {
+                               subList.map((subNav, idx) => (
+                                //    console.log(subNav)
+                                
+                                   subNav.sub2.map(subList => (
+                                    <li key={subList.id}>
+                                       <a href="#">{subList.desc}</a>
+                                   </li>
+                                   ))
                                   
                                ))
                            }
